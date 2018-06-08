06e15df87a79481a69f76519bb1fd29912475c7d
%%GITNAME%% <%%GITEMAIL%%>
2018-06-08 21:31:30 +0800

    regular commit

--- dsl/test_view_folder.groovy
+++ dsl/test_view_folder.groovy
@@ -2,3 +2,13 @@ folder('TEST_FOLDER/f1/f2') {
     
     description('Folder containing all QA jobs for project A')
 }
+
+folder('TEST_FOLDER/f1') {
+    
+    description('Folder containing all QA jobs for project A')
+}
+
+folder('TEST_FOLDER/f1/f2') {
+    
+    description('Folder containing all QA jobs for project A')
+}
