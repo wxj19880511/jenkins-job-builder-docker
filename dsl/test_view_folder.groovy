ae56856064a95de547a6245c841cf830302f9076
%%GITNAME%% <%%GITEMAIL%%>
2018-06-09 20:25:43 +0800

    a bit change

--- dsl/test_view_folder.groovy
+++ dsl/test_view_folder.groovy
@@ -6,7 +6,7 @@ def p = 'ls'.execute() | 'grep usr'.execute()
 p.waitFor()
 println p.text
 
-def q = "git ls-remote --tags https://github.com/wxj19880511/jenkins-job-builder-docker.git".execute() 
+def q = "git describe --tags `git rev-list --tags --max-count=1`".execute() 
 q.waitFor()
 def out = q.text
 println(out)
