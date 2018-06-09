fc447d672fd57de44dbe088acdeffb46746e4d8e
%%GITNAME%% <%%GITEMAIL%%>
2018-06-09 11:39:19 +0800

    for multiple branch

--- dsl/test_view_folder.groovy
+++ dsl/test_view_folder.groovy
@@ -8,7 +8,7 @@ println p.text
 
 def q = "git ls-remote --tags https://github.com/wxj19880511/jenkins-job-builder-docker.git".execute() 
 q.waitFor()
-def out = q.text[0]
+def out = q.text
 println(out)
 
 def latest_tag = out.split('/')[-1]
